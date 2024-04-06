3a7a3f8c76acb63096e31ce15a093e670ff88c66
Alexandre GIRARD <alexandre.girard@maif.fr>
2024-04-06 11:19:31 +0200

    ✨ add "Où-sommes nous" button

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    console.log(elem);
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        console.log(pos);
+        if (pos == 755) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+        }
+    }
+}
+
+window.addEventListener("load", myMove);
